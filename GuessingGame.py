1224183b080e8da655982749458c5e4e3e2216dc
DeveshSood <77494434+DeveshSood@users.noreply.github.com>
2021-09-16 21:46:13 +0530

    Add files via upload

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,16 +1,16 @@
 number = 9
 chance = 5
 while chance > 0:
-    guess = int(input("Guess A Number Between 1 And 9 :- "))
+    guess = int(input("Guess A Number Between 1 And 9, You Have 5 Lives :- "))
     if guess == number:
         break
     elif guess > number:
-        print("Sorry!! the number is less than you Guess")
+        print("Sorry!! the number is less than you Guess, you now have "+(str(chance-1))+" lives.")
         chance = chance - 1
     elif guess < number:
-        print("Sorry!! the number is greater than you Guess")
+        print("Sorry!! the number is greater than you Guess, you now have "+(str(chance-1))+" lives.")
         chance = chance - 1
 if chance == 0:
-    print("Sorry!! You lost, the number was - ", number)
+    print("Sorry!! You lost, the number was - " +(str(number)))
 if chance > 0:
-    print("WOW!! You got it rigt, the number was - ", number)
+    print("WOW!! You got it rigt, the number was - "+(str(number)))
